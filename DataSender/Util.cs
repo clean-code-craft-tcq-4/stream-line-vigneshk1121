f2bc98211aeaf86a720e5c925b6418abd2a9f063
Patange Pradnya Prasad (SX/EMS2) <ptp2kor@bosch.com>
2022-10-19 19:34:08 +0530

    adding logs to chk output

--- DataSender/Util.cs
+++ DataSender/Util.cs
@@ -6,8 +6,8 @@ namespace DataSender
     public static class Util
     {
         public static string ConvertListToJSON(dynamic input)
-        {
-			Console.WriteLine("printing battery status >>> JsonSerializer.Serialize(input)");
+        
+			Console.WriteLine("printing battery status >>>"+ JsonSerializer.Serialize(input));
             return JsonSerializer.Serialize(input);
         }
     }
